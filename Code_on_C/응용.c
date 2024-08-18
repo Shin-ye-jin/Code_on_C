4d3d4f8a42d6a292c784d2bbc69be23a109e7b40
S510U <fx0605@naver.com>
2024-08-18 21:59:43 +0900

    // 0818

--- Code_on_C/응용.c
+++ Code_on_C/응용.c
@@ -1,6 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<string.h>
 
 int main(void)
 {
+	int year, month, day, birth;
 
+	scanf("%d", &birth);
+
+	day = birth % 100;
+	month = (birth / 100) % 100;
+	year = month / 100;
+
+	if (month >= 3 && month <= 5)
+		printf("당신의 생일은 봄입니다.");
+	else if (month >= 6 && month <= 8)
+		printf("당신의 생일은 여름입니다.");
+	else if (month >= 9 && month <= 11)
+		printf("당신의 생일은 가을입니다.");
+	else
+		printf("당신의 생일은 겨울입니다.");
+
+	return 0; // 1096 내 생일의 계절은?
 }
