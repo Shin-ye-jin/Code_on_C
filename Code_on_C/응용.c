c64cb42a46671429cd76edd2ff3fad7fd15602c8
S510U <fx0605@naver.com>
2024-08-28 21:27:32 +0900

    // 0828

---
+++ Code_on_C/응용.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+//int main(void)
+//{
+//	int n, i, sum = 0;
+//	scanf("%d", &n);
+//
+//	for (i = 0; i <= n; i++) {
+//		if (i % 2 == 0 || i % 5 == 0)
+//			sum += i;
+//	}
+//
+//	printf("2의 배수와 5의 배수의 합은 %d 입니다.", sum);
+//	return 0; // 1104 2의 배수 또는 5의 배수의 합 구하기
+//}
+
+//int main(void)
+//{
+//	char n[100];
+//	int count = 0;
+//
+//	while (1) {
+//		scanf("%s", n);
+//		if (strcmp(n, "1234") == 0) {
+//			printf("열림\n");
+//			break;
+//		}
+//		else {
+//			printf("다시 입력하세요.\n");
+//			count++;
+//		}
+//
+//		if (count == 3)
+//			break;
+//	}
+//	return 0; // 1105 도어락을 열어라
+//}
