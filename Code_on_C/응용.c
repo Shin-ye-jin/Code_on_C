ae389cc08abc51316a351db13a875c297d4f8359
S510U <fx0605@naver.com>
2024-12-16 21:36:44 +0900

    12-16

--- Code_on_C/응용.c
+++ Code_on_C/응용.c
@@ -28,3 +28,34 @@
 //	printf("저의 이름은 %s 이고, 저의 혈액형은 %s 형이며, 저의 취미는 %s입니다.", name, blood, ho);
 //	return 0; // 1116 입학식 자기소개
 //}
+
+//int main(void)
+//{
+//	int a, b, n1, n2;
+//	scanf("%d %d", &a, &b);
+//
+//	n1 = a & b;
+//	n2 = a | b;
+//
+//	if (n1 > n2) printf("%d", n1);
+//	else printf("%d", n2);
+//
+//	return 0; // 1118 비트 연산 프로그램
+//}
+
+//#include<string.h>
+//
+//int main(void)
+//{
+//	char f[20];
+//	float re;
+//	scanf("%s", f);
+//	scanf("%f", &re);
+//	if ((strcmp("망고", f) == 0 || strcmp("수박", f) == 0 || strcmp("사과", f) == 0)) { // 문자열 비교
+//		printf("%.0f", re * 0.9);
+//	}
+//	else {
+//		printf("할인 품목이 아닙니다.");
+//	}
+//	return 0; // 1119 얼마나 저렴할까?
+//}
